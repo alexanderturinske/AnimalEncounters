5e7539ef6e1f0a44bc415a424d68ab13d016a3c2
Alexander James Turinske <a.turinske@gmail.com>
2016-04-04 15:35:49 -0700

    Add cors to handle CORS requests. Server connects to angular, but there is a bug with the file serving of dependencies

--- server/router.js
+++ server/router.js
@@ -1,17 +1,20 @@
 var express = require('express');
+var cors = require('cors');
 var router = express.Router();
+router.use(cors());
 
-router.use('/', express.static(__dirname + '../client'));
+router.use('/', express.static(__dirname + '/../client'));
+router.use('/node-modules', express.static(__dirname + '/../node-modules'));
 
 router.route('/')
   .options(function(req, res) {
      res.status(200).send('OPTIONS request');
   })
   .get(function(req, res) {
-    res.status(200).send('Get request!');
+    res.status(200).send(__dirname + '/../client');
   })
   .post(function(req, res) {
-    res.status(200).send('Post request!');
+    res.status(200).send('POST request!');
   });
 
 module.exports = router;

eab933873b78c9e386518ff6847c2bb276bc133f
Alexander James Turinske <a.turinske@gmail.com>
2016-04-04 17:40:58 -0700

    Add animals schema to database

--- server/schema.js
+++ server/schema.js
@@ -1,11 +1,28 @@
 var mongoose = require('mongoose');
 var db = require('./database');
 
-var animalSchema = mongoose.Schema({
-  id: Number,
-  name: String
+var animalSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true
+  },
+  encounter: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 var Animal = mongoose.model('Animal', animalSchema);
+
+// test information
+var octopus = new Animal({
+  name: 'Octopus'
+});
+var elephant = new Animal({
+  name: 'Elephant'
+});
+var parrot = new Animal({
+  name: 'African Grey Parrot'
+});
 
 module.exports = Animal;
